18b8860782077d1c12abd7b6f1c6e6e27a33bec4
[Kunal_Joshi] <[joshikunal14@gmail.com]>
2023-02-07 20:09:00 +0530

    Second Commit

--- New_test.py
+++ New_test.py
@@ -1,44 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
+with open('Output' + '.csv', 'a', newline='', encoding='UTF-8-SIG') as fp:
+    header = ["School", "Address", "State", "Zip_Code", "First_name","Last_name", "Job_title", "Phone", "Email"]
+    writer = csv.writer(fp)
+    writer.writerow(header)
 
 def start():
     for i in range(0,9):
         url = f"https://isd110.org/our-schools/laketown-elementary/staff-directory?s=&page={i}"
-        print(url)
         re = requests.get(url)
-        print(re.status_code)
         soup = BeautifulSoup(re.text, "html.parser")
 
         school = soup.title.string.split("|")[-1].strip()
-        print(school)
 
         full_address = [a.text.strip() for a in soup.select("p[class='address']")]
         full_address_new = full_address[-1].split("\n")
 
         address = full_address_new[0].strip()
-        print(address)
         state = full_address_new[1].split(",")[0].strip()
-        print(state)
-        zip = full_address_new[1].split(",")[-1].strip()
-        print(zip)
-        information = soup.find_all("div",class_='first group')
-        name = [i.find("h2",class_='title').text for i in information]
-        print(name)
+        zip_Code = full_address_new[1].split(",")[-1].strip()
 
-        job_title = [i.find("div",class_='field job-title').text.strip() for i in information]
-        print(job_title)
+        information = soup.find_all("div", class_='first group')
+        information_contact = soup.find_all("div", class_='last group')
 
-        information_contact = soup.find_all("div",class_='last group')
-        phone = [i.find("div",class_='field phone').text.strip() for i in information_contact]
-        print(phone)
+        for x,y in zip(information,information_contact):
+            name = x.find("h2", class_='title').text
+            first_name = name.split(',')[0]
+            last_name = name.split(',')[1]
+            job_title = x.find("div", class_='field job-title').text.strip()
+            phone = y.find("div", class_='field phone').text.strip()
+            email = y.find("div", class_='field email').text.strip()
+            output_lst = [school,address,state,zip_Code,first_name,last_name,job_title,phone,email]
+            write_data(output_lst)
 
-        email = [i.find("div",class_='field email').text.strip() for i in information_contact]
-        print(email)
 
-        print("******************************************************************")
+def write_data(output_lst):
+    with open('Output' + '.csv', 'a', newline='', encoding='UTF-8-SIG') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(output_lst)
 
 
 if __name__ == '__main__':
-
     start()
